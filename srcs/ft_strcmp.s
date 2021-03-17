049547c2678ac335364329aa0f8748e030e1d1c8
ckurt <ckurt@student.42lyon.fr>
2021-03-17 14:11:42 +0100

    strcmp done

--- srcs/ft_strcmp.s
+++ srcs/ft_strcmp.s
@@ -4,8 +4,6 @@ _ft_strcmp:
 loop:
 	cmp		[rdi], byte 0
 	je		end
-	cmp		[rsi], byte 0
-	je		end
 	mov		dl, byte [rsi]
 	cmp		byte [rdi], dl
 	jne		end
--- test.c
+++ test.c
@@ -1,10 +1,12 @@
 #include "libasm.h"
+#include <string.h>
 
 int main()
 {
-
+	char	s1[] = "salut";
+	char	s2[] = "salut";
 	// printf("ret : %zu\n", ft_strlen("salut!"));
-	printf("diff : %d\n", strcmp("salut", "sxdut"));
-	printf("diff : %d\n", ft_strcmp("salut", "sxdut"));
+	printf("diff : %d\n", strcmp(s1, s2));
+	printf("diff : %d\n", ft_strcmp(s1,s2));
 	return (0);
 }
