4988eb346dc0cbad15afcc9ae96cf54857191a35
Ckurt <kuroysiris@gmail.com>
2021-03-19 19:40:16 +0100

    added comments

--- srcs/ft_strcmp.s
+++ srcs/ft_strcmp.s
@@ -2,16 +2,16 @@ global	_ft_strcmp
 
 _ft_strcmp:
 loop:
-	cmp		[rdi], byte 0
-	je		end
-	mov		dl, byte [rsi]
-	cmp		byte [rdi], dl
+	cmp		[rdi], byte 0	;check if null byte
+	je		end				;jump to the next section if conditionc checked
+	mov		dl, byte [rsi]	;move the char to a buf
+	cmp		byte [rdi], dl	;compare char with buf char
 	jne		end
 	inc		rdi
-	inc		rsi
-	jmp		loop
+	inc		rsi				;increment both pointers
+	jmp		loop			;loop
 end:
-	movzx	rax, byte [rdi]
-	movzx	rdx, byte [rsi]
-	sub		rax, rdx
+	movzx	rax, byte [rdi]	;mov the letter to rax and fill with zeroes
+	movzx	rdx, byte [rsi]	;mov the letter to a buf and fill with zeroes
+	sub		rax, rdx		;return the diff between each letters
 	ret
